bf751634e81e3d5b97eaf750da4f8fddd48eb232
ariasAleia <ariasfaleja@gmail.com>
2022-07-29 14:48:19 -0500

    Create a constructor and attributes
    New attributes for specific instances were also added

--- scripts/item.py
+++ scripts/item.py
@@ -1,7 +1,15 @@
 class Item:
-    def calculate_total_price(self, x, y):
-        return x * y
-
-
-item1 = Item()
-print(item1.calculate_total_price(5, 6))
+    
+    def __init__(self, name, price, quantity = 0):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        
+    
+item1 = Item("Phone", 50, 6)
+print(f"Name: {item1.name} Price: {item1.price} Quantity: {item1.quantity}")
+item2 = Item("Laptops", 45)
+print(f"Name: {item2.name} Price: {item2.price} Quantity: {item2.quantity}")
+#New attribute
+item2.has_big_screen = False
+print(item2.has_big_screen)
